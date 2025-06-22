743af4aaef30d4bf28e08cca6708e6bb6ec0a340
Oğuzhan Doğdu <oguzhn.dgd@gmail.com>
2025-06-23 01:14:04 +0300

    feat: Implement Movie model and register it in admin

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    movie_id = models.CharField(max_length=50, unique=True, primary_key=True) 
+    title = models.CharField(max_length=255)
+    year = models.IntegerField(null=True, blank=True)
+    director = models.CharField(max_length=255, null=True, blank=True)
+    plot = models.TextField(null=True, blank=True)
+    poster_url = models.URLField(max_length=500, null=True, blank=True)
+    rating = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
